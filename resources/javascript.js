fbf76bff743e411c5b260b4cbddf537cb67bc256
Leo Cheng <leocheng@gmail.com>
2014-08-30 15:23:31 -0700

    Changed How we met

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,7 @@
-// Your Javascript will go here!
+$(document).ready(function) {
+	$("#user-name").click(function() {
+		var newName = window.prompt("Enter a new name:");
+		$("#user-name").text(newName);
+	});
+
+});
